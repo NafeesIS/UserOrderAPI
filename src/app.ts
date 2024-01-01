8c1c38710d0c6c15ab4a1a9c3aa623ebf9ce9f17
NafeesIS <nafeesshawon555@gmail.com>
2024-01-01 19:15:00 +0600

    app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import { userRoute } from "./app/modules/user/user.route";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { userRoute } from './app/modules/user/user.route';
 const app: Application = express();
 
-//parsers
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/users", userRoute);
+app.use('/api/users', userRoute);
 
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        status: true,
-        message: "User Order Server is running",
-    });
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: true,
+    message: 'User Order Server is running',
+  });
 });
 
 export default app;
